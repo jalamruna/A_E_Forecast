90d80950343eeaa258f7a374b78e13189f0d0b6e
LEON RODENAS <pablo.leonrodenas@nhs.net>
2024-07-18 08:56:49 +0100

    Setting up initial project structure adding RTT data from NHS England website

---
+++ 00 Setup project folder structure.R
@@ -0,0 +1,15 @@
+project_setup <-function(){
+  
+  if(!dir.exists("data")){dir.create("data")}
+  if(!dir.exists("Output")){dir.create("Output")}
+  if(!dir.exists("Shapefiles")){dir.create("Shapefiles")}
+  if(!dir.exists("Checks")){dir.create("Checks")}
+  if(!dir.exists("Maps")){dir.create("Maps")}
+  if(!dir.exists("Archive")){dir.create("Archive")}
+  #Create sub-folders within folders. This sub folder is nested under Archive folder
+  if(dir.exists("Archive")){dir.create(("../Rprojects/Rwarehouse/Project/subfolder_new"))}
+  
+} 
+
+# Run code below to use function and create folder structure
+project_setup()
